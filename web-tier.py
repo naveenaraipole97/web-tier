5933d2446ff37503b48c20b1166d003b652ef194
naveena <144768728+naveenaraipole97@users.noreply.github.com>
2024-03-16 22:32:36 -0700

    web-tier update

--- web-tier.py
+++ web-tier.py
@@ -1,54 +1,58 @@
+from io import BytesIO
 from flask import Flask, request
 import csv
 import os
+import json
+from PIL import Image
+from concurrent.futures import ThreadPoolExecutor
+import threading
 import boto3
 from botocore.exceptions import NoCredentialsError, PartialCredentialsError, EndpointConnectionError
-# from line_profiler import LineProfiler
 
 app=Flask(__name__)
 
-csv_file_path='Classification_Results.csv'
-
-Results_dict={}
-
-req_queue_url='https://sqs.us-east-1.amazonaws.com/637423171832/1228052438-req-queue'
-resp_queue_url='https://sqs.us-east-1.amazonaws.com/637423171832/1228052438-resp-queue'
-
-s3=boto3.client('s3')
-sqs=boto3.client('sqs')
-
-def sendMessageToReqQueue(s3_file_path):
-    message_body=s3_file_path
-    print(s3_file_path)
-    response = sqs.send_message(
-        QueueUrl=req_queue_url,
-        MessageBody=message_body
-    )
-    print(f"Message sent to request queue successfully. Message ID: {response['MessageId']}, Message: {response}")
 
 
-def uploadToS3(file_object,bucket_name,s3_file_name):
-    
+#local store for storing messages from response queue
+results_dict=dict()
+
+req_queue_url='https://sqs.us-east-1.amazonaws.com/905418031675/1228052438-req-queue'
+resp_queue_url='https://sqs.us-east-1.amazonaws.com/905418031675/1228052438-resp-queue'
+
+s3_in_bucket="1228052438-in-bucket-1"
+
+s3=boto3.client('s3',region_name="us-east-1")
+sqs=boto3.client('sqs',region_name="us-east-1")
+
+def get_response_once_available(file_name):
     try:
-        s3.upload_fileobj(file_object,bucket_name,s3_file_name)
-        s3_file_path=s3_file_name
-        print(f'File uploaded to S3 Successfully to {s3_file_path}')
-        sendMessageToReqQueue(s3_file_path)
-    except NoCredentialsError:
-        print('Credentials Not Available')
+        while file_name not in results_dict.keys():
+            continue
+        face_name = results_dict[file_name]
+        results_dict.pop(file_name)
+        return face_name
+    except:
+        print("Error while waiting for response from Queue")
 
-def getNameFromFile(csv_file,target_file_name):
-    if not bool(Results_dict):
-        with open(csv_file,'r') as file:
-            reader=csv.DictReader(file)
-            for row in reader:
-                Results_dict[row['Image']]=row['Results']
-    if(Results_dict[target_file_name]):
-        return Results_dict[target_file_name]
-    return "Not Found"
+def sendMessageToReqQueue(image_byte_array):
+    try:
+        response = sqs.send_message(
+            QueueUrl=req_queue_url,
+            MessageBody=image_byte_array
+        )
+        print(f"Message sent to request queue successfully. Message ID: {response['MessageId']}")
+        
+    except NoCredentialsError:
+        print("Credentials not available. Please provide valid AWS credentials.")
+    except PartialCredentialsError:
+        print("Partial credentials provided. Please provide valid AWS credentials.")
+    except EndpointConnectionError:
+        print("Error connecting to the SQS endpoint. Please check your network connectivity or endpoint configuration.")
+    except Exception as e:
+        print(f"An error occurred: {str(e)}")
 
 # check how this works for concurrent requests
-def getResponseFromRespQueue():
+def getResponseFromRespQueue(): #fix the return of this
     while True:
         try:
             response = sqs.receive_message(
@@ -59,8 +63,8 @@ def getResponseFromRespQueue():
                 MessageAttributeNames=[
                     'All'
                 ],
-                MaxNumberOfMessages=1,
-                VisibilityTimeout=900,
+                MaxNumberOfMessages=10,
+                VisibilityTimeout=10,
                 WaitTimeSeconds=20
             )
     
@@ -70,29 +74,16 @@ def getResponseFromRespQueue():
                 for message in messages:
                     print(f"Received message: Message body {message['Body']}")
                     result=message['Body']
-    
+                    file_name,face_name=result.split(':')
+                    results_dict[file_name]=result
+                    
                     receipt_handle=message['ReceiptHandle']
-
-                    # response = sqs.get_queue_attributes(
-                    #     QueueUrl=resp_queue_url,
-                    #     AttributeNames=['ApproximateNumberOfMessages']
-                    # )
-                    # print(int(response['Attributes']['ApproximateNumberOfMessages']));
-
                     sqs.delete_message(
                         QueueUrl=resp_queue_url,
                         ReceiptHandle=receipt_handle
                     )
-
-                    # response = sqs.get_queue_attributes(
-                    #     QueueUrl=resp_queue_url,
-                    #     AttributeNames=['ApproximateNumberOfMessages']
-                    # )
-                    # print(int(response['Attributes']['ApproximateNumberOfMessages']));
-            else:
-                print("No messages received. Waiting for messages...")
-            
-            return result
+            # else:
+                # print("No messages received. Waiting for messages...")
         
         except NoCredentialsError:
             print("Credentials not available. Please provide valid AWS credentials.")
@@ -103,47 +94,45 @@ def getResponseFromRespQueue():
         except Exception as e:
             print(f"An error occurred: {str(e)}")  
 
-        
 
-# @lp.profile
+# Establishing an independent thread to manage ongoing polling from the Response Queue.
+sqs_monitoring_thread = threading.Thread(target=getResponseFromRespQueue)
+sqs_monitoring_thread.start()
+
+        
+# num_max_workers = 100
 @app.route('/',methods=['POST'])
 def post_data():
     if 'inputFile' not in request.files:
         return 'No File Uploaded, please upload!'
 
     file=request.files['inputFile']
-    # fileName, extension=os.path.splitext(file.filename)
-    s3_key=file.filename
 
-    #Uploading the file to S3 input bucket
-    uploadToS3(file,"1228052438-in-bucket",s3_key)
-
+    # sending image as byte array to sqs request queue
+    image_bytes = file.read()
     
-    response = getResponseFromRespQueue()
+    message = {
+        "key": file.filename,
+        "value": image_bytes.decode('latin-1')
+    }
 
-    print('Here')
-    print(response)
 
-    # sqs.delete_message(
-    #     QueueUrl=resp_queue_url,
-    #     ReceiptHandle=receipt_handle
-    # )
-    
-    #change this acc to part2
-    # result=getNameFromFile(csv_file_path,fileName)
-    # response_data=fileName+':'+result
-    # response_data=response_data.replace('"','')
-    # response_data=response_data.strip()
-    # if response:
+    sendMessageToReqQueue(json.dumps(message))
+
+    file_name, extension=os.path.splitext(file.filename)
+
+    response = get_response_once_available(file_name)
+
     return response
 
-# lp.add_profile(post_data)
-
+    # with ThreadPoolExecutor(max_workers = num_max_workers) as executor:
+    #     response = getResponseFromRespQueue(s3_file_name)
+    #     print("returning - ", response)
+     #check this
+    
 def main():
-    app.run(host='0.0.0.0', port=80, threaded=True)
+    app.run(host='0.0.0.0', port=8000, threaded=True)
      
 if __name__=='__main__':
-#    lp(post_data)()
-#    lp.print_stats()
    main()
    
